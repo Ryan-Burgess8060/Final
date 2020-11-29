df2f746446eb909b97c5e8543082ff4a85388b88
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2020-11-29 01:19:19 -0500

    Add files via upload

--- Index.php
+++ Index.php
@@ -14,28 +14,33 @@
 		<label for="x1">x1</label><br>
 		<input type="text" id="x1" name="x1"><br>
 		<label for="y1">y1</label><br>
-		<input type="text" id="y1" name="y1">
+		<input type="text" id="y1" name="y1"><br>
 		<label for="x2">x2</label><br>
 		<input type="text" id="x2" name="x2"><br>
 		<label for="y2">y2</label><br>
-		<input type="text" id="y2" name="y2">
+		<input type="text" id="y2" name="y2"><br>
 		<input type="submit" name="submit" value="submit" />
 		<?php
 		if(isset($_POST['submit'])) {
-			$postRequest = array(
-				'x1' => $_POST['x1'],
-				'y1' => $_POST['y1'],
-				'x2' => $_POST['x2'],
-				'y2' => $_POST['y2']
-			);
+			if(!empty($_POST['x1']) && !empty($_POST['y1']) && !empty($_POST['x2']) && !empty($_POST['y2'])){
+				$postRequest = array(
+					'x1' => $_POST['x1'],
+					'y1' => $_POST['y1'],
+					'x2' => $_POST['x2'],
+					'y2' => $_POST['y2']
+				);
 
-			$cURLConnection = curl_init('https://us-east1-capable-arbor-286903.cloudfunctions.net/function-final');
-			curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, $postRequest);
-			curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+				$cURLConnection = curl_init('https://us-east1-capable-arbor-286903.cloudfunctions.net/function-final');
+				curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, $postRequest);
+				curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
 
-			$apiResponse = curl_exec($cURLConnection);
-			curl_close($cURLConnection);
-			echo $apiResponse;
+				$apiResponse = curl_exec($cURLConnection);
+				curl_close($cURLConnection);
+				echo $apiResponse;
+			}
+			else {
+				echo "The inputs aren't being picked up";
+			}
 		}
 		?>
 	</form>
