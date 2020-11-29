0eae55c2fc0a8d402d33a255170da2f54be3a356
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2020-11-28 21:31:23 -0500

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<!-- #Ryan Burgess
+#11/28/2020
+#Index.php -->
+<head>
+	<title>Burgess Final Project</title>
+	<meta charset="utf-8"/>
+	<!--<link href="style.css" rel="stylesheet"/>-->
+</head>
+<body>
+	<p>Please input 2 points for corners of a square.</p>
+	<form method="post">
+		<label for="x1">x1</label><br>
+		<input type="text" id="x1" name="x1"><br>
+		<label for="y1">y1</label><br>
+		<input type="text" id="y1" name="y1">
+		<label for="x2">x2</label><br>
+		<input type="text" id="x2" name="x2"><br>
+		<label for="y2">y2</label><br>
+		<input type="text" id="y2" name="y2">
+		<input type="submit" name="submit" value="submit" />
+	</form>
+</body>
