63e9cecca0e4fbeeddecd9c0efaa4f2b1e9671b7
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2020-12-06 23:45:30 -0500

    Add files via upload

--- Index.php
+++ Index.php
@@ -37,7 +37,9 @@
 			xhttp.open("POST", 'https://us-east1-capable-arbor-286903.cloudfunctions.net/function-final', true);
 			xhttp.setRequestHeader("Content-type", "application/json");
 			xhttp.send(JSON.stringify({x1:xval1, y1:yval1, x2:xval2, y2:yval2}));
+			console.log(document.getElementById('answer'));
 			console.log("bye");
+			
 		}
 	</script>
 </body>
